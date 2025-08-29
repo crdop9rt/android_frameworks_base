524bf22adbbdc9d4ab80a42c3067479e01f6e81f
Pranav Vashi <neobuddy89@gmail.com>
2025-08-29 20:52:00 +0530

    PixelPropsUtils: Fix props for s24 ultra
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -240,9 +240,8 @@ public final class PixelPropsUtils {
         propsToChangeBS4.put("MODEL", "2SM-X706B");
         propsToChangeBS4.put("MANUFACTURER", "blackshark");
         propsToChangeS24U = new HashMap<>();
-        propsToChangeS24U.put("BRAND", "SAMSUNG");
-        propsToChangeS24U.put("DEVICE", "S24 ULTRA");
-        propsToChangeS24U.put("MANUFACTURER", "SM-S928B");
+        propsToChangeS24U.put("BRAND", "samsung");
+        propsToChangeS24U.put("MANUFACTURER", "samsung");
         propsToChangeS24U.put("MODEL", "SM-S928B");
         
     }
