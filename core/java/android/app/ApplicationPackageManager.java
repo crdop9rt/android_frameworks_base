3cd7a94a781d8bbd4eeee8b38eaa3d3b605de343
Pranav Vashi <neobuddy89@gmail.com>
2025-08-30 09:39:56 +0530

    PixelPropsUtils: Update tensor codes for P10
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -837,6 +837,9 @@ public class ApplicationPackageManager extends PackageManager {
             };
 
     private static final String[] pTensorCodenames = {
+            "blazer",
+            "frankel",
+            "mustang",
             "tegu",
             "comet",
             "komodo",
